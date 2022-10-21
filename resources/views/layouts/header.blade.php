54e7a7a5bf07a8ff160756877e4ca57cafe1a2b4
fauzansubagja <fauzansubagja10@gmail.com>
2022-10-21 14:16:19 +0700

    update route

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,12 +2,12 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
 	<title>DSPay | SMKN4BDG</title>
-	<link rel="shortcut icon" type="image/x-icon" href="app/img/dspay-logo.png">
-	<link rel="stylesheet" href="{{ asset('app/css/bootstrap.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('app/plugins/fontawesome/css/fontawesome.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('app/plugins/fontawesome/css/all.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('app/css/feathericon.min.css') }}">
+	<link rel="shortcut icon" type="image/x-icon" href="{{ asset('app/img/dspay-logo.png') }}">
+	<link rel="stylesheet" href="{{ asset('../app/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('../app/plugins/fontawesome/css/fontawesome.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('../app/plugins/fontawesome/css/all.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('../app/css/feathericon.min.css') }}">
 	<link rel="stylehseet" href="https://cdn.oesmith.co.uk/morris-0.5.1.css">
-	<link rel="stylesheet" href="{{ asset('app/plugins/morris/morris.css') }}">
-	<link rel="stylesheet" href="{{ asset('app/css/style.css') }}">
+	<link rel="stylesheet" href="{{ asset('../app/plugins/morris/morris.css') }}">
+	<link rel="stylesheet" href="{{ asset('../app/css/style.css') }}">
 </head>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PembayaranController;
+use App\Http\Controllers\ManajemenController;
+use App\Http\Controllers\KelasController;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\KelulusanController;
+use App\Http\Controllers\KenaikanController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,38 +19,40 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/admin/pembayaran', [PembayaranController::class, 'index']);
+Route::get('/management/tahunajaran', [ManajemenController::class, 'index']);
+Route::get('/management/kelas', [KelasController::class, 'index']);
+Route::get('/management/siswa', [SiswaController::class, 'index']);
+Route::get('/management/kelulusan', [KelulusanController::class, 'index']);
+Route::get('/management/kenaikan', [KenaikanController::class, 'index']);
+Route::get('/admin/user', [UserController::class, 'index']);
+Route::get('/admin/laporan/rekapitulasi', [RekapitulasiController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function () {
-    return view('home');
-});
-Route::get('/pembayaran', function () {
-    return view('admin.pembayaran');
-});
-Route::get('/user', function () {
-    return view('admin.user');
-});
-Route::get('/tahunajaran', function () {
-    return view('admin.manajemen.tahunajaran');
-});
-Route::get('/kelas', function () {
-    return view('admin.manajemen.kelas');
-});
-Route::get('/siswa', function () {
-    return view('admin.manajemen.siswa');
-});
-Route::get('/kelulusan', function () {
-    return view('admin.manajemen.kelulusan');
-});
-Route::get('/kenaikan', function () {
-    return view('admin.manajemen.kenaikan');
-});
-Route::get('/rekapitulasi', function () {
-    return view('admin.laporan.rekapitulasi');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/user', function () {
+//     return view('admin.user');
+// });
+// Route::get('/tahunajaran', function () {
+//     return view('admin.manajemen.tahunajaran');
+// });
+// Route::get('/kelas', function () {
+//     return view('admin.manajemen.kelas');
+// });
+// Route::get('/siswa', function () {
+//     return view('admin.manajemen.siswa');
+// });
+// Route::get('/kelulusan', function () {
+//     return view('admin.manajemen.kelulusan');
+// });
+// Route::get('/kenaikan', function () {
+//     return view('admin.manajemen.kenaikan');
+// });
+// Route::get('/rekapitulasi', function () {
+//     return view('admin.laporan.rekapitulasi');
+// });
 
 // belum dikerjakan
 // setting
